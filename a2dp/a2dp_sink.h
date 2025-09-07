2e2207e78d5ac52d18250722dfa0d57d5587342b
fanyx <xmsszycz@163.com>
2025-09-07 17:40:17 +0800

    init

---
+++ a2dp/a2dp_sink.h
@@ -0,0 +1,7 @@
+#ifndef __A2DP_SINK_H__ 
+#define __A2DP_SINK_H__
+
+void a2dp_sink_packet_handler(uint8_t packet_type, uint16_t channel, uint8_t * packet, uint16_t event_size);
+void handle_l2cap_media_data_packet(uint8_t seid, uint8_t *packet, uint16_t size);
+
+#endif 
